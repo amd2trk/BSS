e5d3d2776e24590a0dab108acc6367012f0f32d6
amd2trk <ahmed.tarek.20032013@gmail.com>
2025-03-17 11:44:50 +0200

    Update project (excluded build files)

---
+++ build/CMakeFiles/bss_executable.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ahmed/Dev/BSS/customer_service.cpp" "CMakeFiles/bss_executable.dir/customer_service.cpp.o" "gcc" "CMakeFiles/bss_executable.dir/customer_service.cpp.o.d"
+  "/home/ahmed/Dev/BSS/customer_service_choice.cpp" "CMakeFiles/bss_executable.dir/customer_service_choice.cpp.o" "gcc" "CMakeFiles/bss_executable.dir/customer_service_choice.cpp.o.d"
+  "/home/ahmed/Dev/BSS/main.cpp" "CMakeFiles/bss_executable.dir/main.cpp.o" "gcc" "CMakeFiles/bss_executable.dir/main.cpp.o.d"
+  "/home/ahmed/Dev/BSS/teller.cpp" "CMakeFiles/bss_executable.dir/teller.cpp.o" "gcc" "CMakeFiles/bss_executable.dir/teller.cpp.o.d"
+  "/home/ahmed/Dev/BSS/vip_customer_service.cpp" "CMakeFiles/bss_executable.dir/vip_customer_service.cpp.o" "gcc" "CMakeFiles/bss_executable.dir/vip_customer_service.cpp.o.d"
+  "" "bss_executable" "gcc" "CMakeFiles/bss_executable.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/bss_executable.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bss_executable.dir/link.d"
+  "CMakeFiles/bss_executable.dir/customer_service.cpp.o"
+  "CMakeFiles/bss_executable.dir/customer_service.cpp.o.d"
+  "CMakeFiles/bss_executable.dir/customer_service_choice.cpp.o"
+  "CMakeFiles/bss_executable.dir/customer_service_choice.cpp.o.d"
+  "CMakeFiles/bss_executable.dir/main.cpp.o"
+  "CMakeFiles/bss_executable.dir/main.cpp.o.d"
+  "CMakeFiles/bss_executable.dir/teller.cpp.o"
+  "CMakeFiles/bss_executable.dir/teller.cpp.o.d"
+  "CMakeFiles/bss_executable.dir/vip_customer_service.cpp.o"
+  "CMakeFiles/bss_executable.dir/vip_customer_service.cpp.o.d"
+  "bss_executable"
+  "bss_executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bss_executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
